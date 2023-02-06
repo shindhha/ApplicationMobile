ac8bc2907d9b128ba8f308b605eee09e63e39198
guillaume.medard <guillaume.medard@iut-rodez.fr>
2023-02-06 17:17:07 +0100

    Fin TP3 exo 2

--- src/main/java/com/example/medisoft/MainActivity.java
+++ src/main/java/com/example/medisoft/MainActivity.java
@@ -1,22 +1,51 @@
 package com.example.medisoft;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.lang.reflect.Array;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    TextView resultat;
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView resultat = findViewById(R.id.result);
-        String text = getString(R.string.result);
+        resultat = findViewById(R.id.result);
+        text = getString(R.string.result);
 
-        int nbAleatoire = OutilAleatoire.tirageAleatoire(99);
+
+    }
+
+
+    public void generNbAleatoire(View view) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    public void pileFace(View view) {
+        String nbAleatoire = OutilAleatoire.tiragePileFace();
         resultat.setText(text + nbAleatoire);
+    }
+
+    public void LancerDe(View view) {
+        int nbAleatoire = OutilAleatoire.tirageAleatoire(6);
+        resultat.setText(text + (nbAleatoire+1));
+    }
+
+    public void Loto(View view) {
+        int nbAleatoire = OutilAleatoire.tirageAleatoire(49);
+        resultat.setText(text + (nbAleatoire+1) );
     }
 }
