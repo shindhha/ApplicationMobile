8d8beb68dc336d7bac75a00fe6e1dee9c5cd0668
guillaume.medard <guillaume.medard@iut-rodez.fr>
2023-02-20 15:44:05 +0100

    Debut tp Utilisation des listes.

--- src/main/java/com/example/medisoft/MainActivity.java
+++ src/main/java/com/example/medisoft/MainActivity.java
@@ -22,40 +22,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
 
 
+    public void clicAxeA(View view) {
     }
 
     @Override
     public void onClick(View view) {
-        EditText bdTransport = findViewById(R.id.bdTransport);
-        EditText bdNuit =  findViewById(R.id.bdNuit);
-        EditText bdRepas = findViewById(R.id.bdRepas);
-        CheckBox repas2 = findViewById(R.id.repas2);
-        EditText bdVisite = findViewById(R.id.bdVisite);
-        RadioGroup radioGroup = findViewById(R.id.RadioGroup);
-        RadioButton nbJourButton = findViewById(radioGroup.getCheckedRadioButtonId());
-        int nbJour = toInt(nbJourButton.getText());
 
-        int totalNuit = nbJour * toInt(bdNuit.getText());
-        int totalRepas = nbJour * toInt(bdRepas.getText());
-        if (repas2.isActivated()) {
-            totalRepas += toInt(bdRepas.getText());
-        }
-        int totalVisite = nbJour * toInt(bdVisite.getText());
-
-        EditText estimation = findViewById(R.id.ec);
-        estimation.setText(totalNuit + totalRepas + totalVisite + toInt(bdTransport.getText()));
-    }
-    private int toInt(CharSequence c) {
-        return Integer.getInteger(c.toString());
     }
 
-    public void clear(View view) {
+    public void clicTous(View view) {
     }
 
-    public void calcul(View view) {
+    public void clicTroncCommun(View view) {
+    }
 
+    public void clicAxeB(View view) {
     }
 }
