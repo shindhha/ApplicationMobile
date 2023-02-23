a3c230ce4652dadd26aa8d00b0f6f376c71eb687
guillaume.medard <guillaume.medard@iut-rodez.fr>
2023-02-23 09:16:14 +0100

    Suite TP listes.

--- src/main/java/com/example/medisoft/MainActivity.java
+++ src/main/java/com/example/medisoft/MainActivity.java
@@ -5,41 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
-import java.lang.reflect.Array;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    TextView resultat;
-    String text;
+    private ListView listView;
+    private String[] troncCommun;
+    private String[] parcourA;
+    private String[] parcourB;
+    private List<String> all;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.liste);
 
+        troncCommun = getResources().getStringArray(R.array.tronc_commun);
+
+        parcourA =getResources().getStringArray(R.array.parcours_A);
+
+        parcourB = getResources().getStringArray(R.array.parcours_B);
+
+        all = new ArrayList<>();
+        all.addAll(Arrays.asList(troncCommun));
+        all.addAll(Arrays.asList(parcourA));
+        all.addAll(Arrays.asList(parcourB));
     }
 
 
 
     public void clicAxeA(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,parcourA);
+        listView.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View view) {
-
     }
 
     public void clicTous(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,all);
+        listView.setAdapter(adapter);
     }
 
     public void clicTroncCommun(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,troncCommun);
+        listView.setAdapter(adapter);
     }
 
     public void clicAxeB(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,parcourB);
+        listView.setAdapter(adapter);
     }
 }
